81869b181f5966a08397f3888fa75ffdc32cca21
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2024-02-29 17:05:04 +0700

    add customer orderfood UI

---
+++ project/resources/views/tablemanagement.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.chief-layouts')
+
+@section('title', 'Example Page')
+
+@section('content')
+<h1 class="prompt pb-5 font-extralight text-2xl">โต๊ะทั้งหมด</h1>
+    <div class="flex flex-row justify-start gap-4">
+
+    </div>
+@endsection

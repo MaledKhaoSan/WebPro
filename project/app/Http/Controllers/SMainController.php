7ed962328bd0070bed85eed2ec01d98baf560a76
ilSPRITEli <gvkgodvilekung@gmail.com>
2024-03-02 21:13:11 +0700

    ww

--- project/app/Http/Controllers/SMainController.php
+++ project/app/Http/Controllers/SMainController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Transaction;
 use App\Models\Menu;
 use App\Models\Order;
+use App\Models\Table;
 
 date_default_timezone_set('Asia/Bangkok');
 
@@ -16,19 +17,29 @@ class SMainController extends Controller
     {
         // get only today trans using trans_date field
         $trans = Transaction::whereDate('trans_date', date("Y-m-d"))->get();
+        // get only yesterday trans using trans_date field
+        $yesterday = date("Y-m-d", strtotime("-1 days"));
+        $trans_ = Transaction::whereDate('trans_date', $yesterday)->get();
+        
+
         // sum the number of amount of transactions
         $total = $trans->sum('amount');
+        $total_ = $trans_->sum('amount');
+        $raises = number_format(($total * 100 / $total_) - 100, 2);
         // dd($trans);
         $tops = Menu::orderBy('sales', 'desc')->take(3)->get();
         // dd($tops);
         // sum the number of pending order
         $pending = Order::where('order_status', 'pending')->count();
-
+        // sum the number of avail table
+        $avail = Table::where('table_status', 'avail')->count();
 
         return view('staff-main', [
             'total' => $total,
+            'raises' => $raises,
             'tops' => $tops,
             'pending' => $pending,
+            'avail' => $avail
         ]);
     }
 
